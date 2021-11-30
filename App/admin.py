ec650ca45159ec1e8258614e8a55c7a23868b07d
Emanuel Gonzalez <emanuel.gonzalez0520@gmail.com>
2021-11-30 16:07:17 -0300

    Carga de modelos al admin

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from App.models import Cliente, Profesional, Entrega 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Profesional)
+admin.site.register(Entrega)
