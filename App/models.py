b97fc59e07718735096b56016987f283e22f6928
Emanuel Gonzalez <emanuel.gonzalez0520@gmail.com>
2021-12-17 20:14:04 -0300

    Agregue metodo para que en los objetos generados en la BD se vean un poco mejor

--- App/models.py
+++ App/models.py
@@ -8,6 +8,9 @@ class Cliente(models.Model):
     telefono = models.IntegerField()
     cuit = models.IntegerField()
     
+    def __str__(self):
+        return f'Nombre: {self.nombre} - Apellido: {self.apellido} - Telefono: {self.telefono} - Cuit: {self.cuit}'
+    
 class Profesional(models.Model):
     
     nombre = models.CharField(max_length=40)
@@ -15,9 +18,15 @@ class Profesional(models.Model):
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
     
+    def __str__(self):
+        return f'Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email} - Profesión: {self.profesion}'
+    
 class Entrega(models.Model):
     
     nombre_entrega = models.CharField(max_length=50)
     fechadevencimiento = models.DateField()
     entregado = models.BooleanField()
+    
+    def __str__(self):
+        return f'Nombre de entrega: {self.nombre_entrega} - Fecha límite: {self.fechadevencimiento} - Entregado: {self.entregado}'
 
